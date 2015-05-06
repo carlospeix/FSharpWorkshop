5357f9dbbcada165b0a59985f49e08eb1aa29d4c
Carlos Peix <carlos.peix@gmail.com>
2015-05-06 12:03:22 -0300

    Segundo modulo resuelto

--- Module2/1.Pre/Application/Functions.fs
+++ Module2/1.Pre/Application/Functions.fs
@@ -9,3 +9,13 @@ let tryPromoteToVip (customer, spendings) =
 let getSpendings customer =
     if customer.Id % 2 = 0 then (customer, 120.0)
     else (customer, 80.0)
+
+let increaseCredit condition customer =
+    if condition customer then { customer with Credit = customer.Credit + 100.0 }
+    else { customer with Credit = customer.Credit + 50.0 }
+
+let vipCondition customer = customer.IsVip
+
+let increaseCreditUsingVip = increaseCredit vipCondition
+
+let upgradeCustomer = getSpendings >> tryPromoteToVip >> increaseCreditUsingVip
